0f2e28b1671ce2e8d76c0fe49e9e06fb3d6c7a89
miyayuu06 <yunafermi@gmail.com>
2025-07-16 16:45:00 +0200

    Improved user interface

--- chip8Emulator/src/main.cpp
+++ chip8Emulator/src/main.cpp
@@ -9,22 +9,24 @@ using namespace chip8;
 
 int main()
 {
+
+    std::cout << "Welcome to the Chip-8 emulator!\n\n\n";
+
     Chip8 chip8;
-    int programDataSize = chip8.load_rom("C:/Users/yunaf/Desktop/pong.ch8");
-    //int programDataSize = chip8.load_rom("C:/Users/yunaf/Desktop/2-ibm-logo.ch8");
-    //int programDataSize = chip8.load_rom("C:/Users/yunaf/Desktop/3-corax+.ch8");
-    //int programDataSize = chip8.load_rom("C:/Users/yunaf/Desktop/7-beep.ch8");
+    int programDataSize = 0;
+
+    while (!programDataSize) {
+        std::string programNameLink;
+        std::cout << "Introduce valid program: ";
+        std::cin >> programNameLink;
+        std::cout << std::endl;
+
+        programDataSize = chip8.load_rom(programNameLink);
+
+    }
+
+    std::cout << "ROM Loading succesful!\n";
+
     chip8.program(programDataSize);
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
